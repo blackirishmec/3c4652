03490334e61102ad4145f6c0973db891362bd73b
Michael Coughlin <blackirishmec@gmail.com>
2025-03-04 13:08:31 -0500

    Refactor code formatting and improve VSCode settings for Prettier and ESLint integration

--- frontend-challenge/src/nodes/PositionLoggerNode.tsx
+++ frontend-challenge/src/nodes/PositionLoggerNode.tsx
@@ -1,25 +1,26 @@
-import { Handle, Position, type NodeProps } from '@xyflow/react';
+import { Handle, Position } from '@xyflow/react';
 
-import { type PositionLoggerNode } from './types';
+import type { PositionLoggerNode } from './types';
+import type { NodeProps } from '@xyflow/react';
 
 export function PositionLoggerNode({
-  positionAbsoluteX,
-  positionAbsoluteY,
-  data,
+	positionAbsoluteX,
+	positionAbsoluteY,
+	data,
 }: NodeProps<PositionLoggerNode>) {
-  const x = `${Math.round(positionAbsoluteX)}px`;
-  const y = `${Math.round(positionAbsoluteY)}px`;
+	const x = `${Math.round(positionAbsoluteX)}px`;
+	const y = `${Math.round(positionAbsoluteY)}px`;
 
-  return (
-    // We add this class to use the same styles as React Flow's default nodes.
-    <div className="react-flow__node-default">
-      {data.label && <div>{data.label}</div>}
+	return (
+		// We add this class to use the same styles as React Flow's default nodes.
+		<div className="react-flow__node-default">
+			{data.label && <div>{data.label}</div>}
 
-      <div>
-        {x} {y}
-      </div>
+			<div>
+				{x} {y}
+			</div>
 
-      <Handle type="source" position={Position.Bottom} />
-    </div>
-  );
+			<Handle type="source" position={Position.Bottom} />
+		</div>
+	);
 }
