1ab08813de7f6ee9498b8a06278ed5312cef72fe
Michael Coughlin <blackirishmec@gmail.com>
2025-03-04 13:16:05 -0500

    Fix destructuring of nodes state in App component

--- frontend-challenge/src/App.tsx
+++ frontend-challenge/src/App.tsx
@@ -17,7 +17,7 @@ import { edgeTypes, initialEdges } from './edges';
 import { initialNodes, nodeTypes } from './nodes';
 
 export default function App() {
-	const [nodes, , onNodesChange] = useNodesState(initialNodes);
+	const [nodes, _, onNodesChange] = useNodesState(initialNodes);
 	const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 	const onConnect: OnConnect = useCallback(
 		connection => setEdges(edgesValue => addEdge(connection, edgesValue)),
