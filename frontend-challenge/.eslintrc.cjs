f75521dcb71bb811ea74aabf32c3e233a4d01d6b
Michael Coughlin <blackirishmec@gmail.com>
2025-03-04 13:00:54 -0500

    Add ESLint and Prettier configurations, update project settings, and format code

--- frontend-challenge/.eslintrc.cjs
+++ frontend-challenge/.eslintrc.cjs
@@ -1,15 +1,180 @@
 module.exports = {
 	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: { jsx: true },
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: './tsconfig.json',
+		tsconfigRootDir: __dirname,
+	},
 	env: { browser: true, es2020: true },
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:react-hooks/recommended',
+		'plugin:import/recommended',
+		'plugin:import/typescript',
 	],
-	ignorePatterns: ['dist', '.eslintrc.cjs'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh'],
+	ignorePatterns: [
+		'dist',
+		'.eslintrc.cjs',
+		'!*.js',
+		'.*.js',
+		'*.json',
+		'*.js.map',
+	],
+	plugins: ['react-refresh', 'import'],
 	rules: {
+		'no-console': 'warn',
+		'no-debugger': 'warn',
+		'no-warning-comments': ['warn', { terms: ['!', 'any other term'] }],
+		'object-shorthand': 'error',
+		'no-param-reassign': [
+			'error',
+			{
+				props: false,
+			},
+		],
+
+		'no-shadow': 'off',
+		'@typescript-eslint/no-shadow': ['error'],
+
+		'no-use-before-define': 'off',
+		'@typescript-eslint/no-use-before-define': [
+			'error',
+			{ functions: false, variables: false },
+		],
+
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+		],
+
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/no-unnecessary-type-assertion': 'error',
+		'@typescript-eslint/no-floating-promises': [
+			'error',
+			{ ignoreVoid: true },
+		],
+		'@typescript-eslint/strict-boolean-expressions': 'warn',
+		'@typescript-eslint/consistent-type-imports': [
+			'error',
+			{
+				prefer: 'type-imports',
+				fixStyle: 'separate-type-imports',
+			},
+		],
+		'@typescript-eslint/ban-ts-comment': 'warn',
+		'@typescript-eslint/ban-types': 'off',
+
+		'import/order': [
+			'error',
+			{
+				alphabetize: { order: 'asc', caseInsensitive: true },
+				groups: [
+					'external',
+					'type',
+					'internal',
+					'builtin',
+					['parent', 'sibling', 'index'],
+					'unknown',
+				],
+				pathGroups: [
+					{
+						pattern: '@(react|react-native)',
+						group: 'external',
+						position: 'before',
+					},
+					{
+						pattern: '@reduxjs/**',
+						group: 'external',
+						position: 'before',
+					},
+					{
+						pattern: 'expo**',
+						group: 'external',
+						position: 'before',
+					},
+					{
+						pattern: 'react-native',
+						group: 'external',
+						position: 'before',
+					},
+					{
+						pattern: '@/types/**',
+						group: 'type',
+						position: 'after',
+					},
+					{
+						pattern: '@/interfaces/**',
+						group: 'type',
+						position: 'after',
+					},
+					{
+						pattern: '@/enums/**',
+						group: 'type',
+						position: 'after',
+					},
+					{
+						pattern: '@/models/**',
+						group: 'type',
+						position: 'after',
+					},
+					{
+						pattern: '@/api/**',
+						group: 'type',
+						position: 'after',
+					},
+					{
+						pattern: '@/redux/**',
+						group: 'type',
+						position: 'after',
+					},
+					{
+						pattern: '@/hooks/**',
+						group: 'type',
+						position: 'after',
+					},
+					{
+						pattern: '@/utilities/**',
+						group: 'type',
+						position: 'after',
+					},
+					{
+						pattern: '@/functions/**',
+						group: 'type',
+						position: 'after',
+					},
+					{
+						pattern: '@/components/**',
+						group: 'type',
+						position: 'after',
+					},
+					{
+						pattern: '@/styles/**',
+						group: 'type',
+						position: 'after',
+					},
+					{
+						pattern: '@/tests/**',
+						group: 'type',
+						position: 'after',
+					},
+					{
+						pattern: '@/**',
+						group: 'type',
+						position: 'after',
+					},
+				],
+				'newlines-between': 'always',
+				pathGroupsExcludedImportTypes: ['builtin', 'type'],
+				named: true,
+			},
+		],
+
 		'react-refresh/only-export-components': [
 			'warn',
 			{ allowConstantExport: true },
--- frontend-challenge/src/App.tsx
+++ frontend-challenge/src/App.tsx
@@ -1,42 +1,43 @@
 import { useCallback } from 'react';
+
 import {
-  ReactFlow,
-  Background,
-  Controls,
-  MiniMap,
-  addEdge,
-  useNodesState,
-  useEdgesState,
-  type OnConnect,
+	addEdge,
+	Background,
+	Controls,
+	MiniMap,
+	type OnConnect,
+	ReactFlow,
+	useEdgesState,
+	useNodesState,
 } from '@xyflow/react';
 
 import '@xyflow/react/dist/style.css';
 
+import { edgeTypes, initialEdges } from './edges';
 import { initialNodes, nodeTypes } from './nodes';
-import { initialEdges, edgeTypes } from './edges';
 
 export default function App() {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const onConnect: OnConnect = useCallback(
-    (connection) => setEdges((edges) => addEdge(connection, edges)),
-    [setEdges]
-  );
+	const [nodes, , onNodesChange] = useNodesState(initialNodes);
+	const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+	const onConnect: OnConnect = useCallback(
+		connection => setEdges(edgesValue => addEdge(connection, edgesValue)),
+		[setEdges],
+	);
 
-  return (
-    <ReactFlow
-      nodes={nodes}
-      nodeTypes={nodeTypes}
-      onNodesChange={onNodesChange}
-      edges={edges}
-      edgeTypes={edgeTypes}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      fitView
-    >
-      <Background />
-      <MiniMap />
-      <Controls />
-    </ReactFlow>
-  );
+	return (
+		<ReactFlow
+			nodes={nodes}
+			nodeTypes={nodeTypes}
+			onNodesChange={onNodesChange}
+			edges={edges}
+			edgeTypes={edgeTypes}
+			onEdgesChange={onEdgesChange}
+			onConnect={onConnect}
+			fitView
+		>
+			<Background />
+			<MiniMap />
+			<Controls />
+		</ReactFlow>
+	);
 }
