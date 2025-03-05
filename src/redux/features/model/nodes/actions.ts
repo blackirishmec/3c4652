516bc1ee9583d209bdd1e1faa0a7afb436b74d47
Michael Coughlin <blackirishmec@gmail.com>
2025-03-05 17:38:24 -0500

    Fix action type for single node fetch in Redux; update 'nodes/nodesFetched' to 'nodes/nodeFetched'

--- src/redux/features/model/nodes/actions.ts
+++ src/redux/features/model/nodes/actions.ts
@@ -2,5 +2,5 @@ import { createAction } from '@reduxjs/toolkit';
 
 import type { Node } from '@/interfaces/models/nodeModels';
 
-export const nodeFetched = createAction<Node>('nodes/nodesFetched');
+export const nodeFetched = createAction<Node>('nodes/nodeFetched');
 export const nodesFetched = createAction<Node[]>('nodes/nodesFetched');
