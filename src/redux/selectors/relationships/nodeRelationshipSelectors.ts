35ae6020eeb4a60fc3ed49800402375083584cb5
Michael Coughlin <blackirishmec@gmail.com>
2025-03-06 12:38:03 -0500

    Add selector to retrieve clicked node's parents with recursive exploration

--- src/redux/selectors/relationships/nodeRelationshipSelectors.ts
+++ src/redux/selectors/relationships/nodeRelationshipSelectors.ts
@@ -13,3 +13,20 @@ export const selectClickedNode = createSelector(
 			? (nodeEntities[clickedNodeId] ?? undefined)
 			: undefined,
 );
+
+export const selectClickedNodeParents = createSelector(
+	[
+		selectClickedNode,
+		(state: RootState) => state.nodes.entities,
+		(state: RootState) => state.edges.entities,
+	],
+	(clickedNode, nodeEntities, edgeEntities): Node[] => {
+		if (clickedNode === undefined) return [];
+
+		const clickedNodeParents: Node[] = [];
+
+		// TODO:  Recursively explore clickedNode.data.prerequisites to get nodes which have ids in clickedNode.data.prerequisites and add them to the clickedNodeParents array (unique values in this array only!). Then recursively explore those added clickedNodeParents in the same manner.
+
+		return clickedNodeParents;
+	},
+);
